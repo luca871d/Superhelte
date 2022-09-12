0408510318e7eec16675e12ef5be4012b1db7be2
luca871d <luca871d@stud.kea.dk>
2022-09-12 15:39:32 +0200

    Adding scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String realNameIn = sc.next();
+        String superNameIn = sc.next();
+
         Superhero hero1 = new Superhero("1", "2", true, "4", 5555, 6.0);
         System.out.println(hero1.toString());
     }
